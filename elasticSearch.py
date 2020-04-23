29d16558722068a5fc6de0f2862359726c73c2ff
Legler <stefanie.legler@nttdata.com>
2020-04-23 19:26:27 +0200

    new elasticSearch

--- elasticSearch.py
+++ elasticSearch.py
@@ -23,24 +23,18 @@ sss = sparkStructuredStreaming.kafka_spark_stream(bootstrap)
 parsedDF = sss.stream_quotes(spark)       
 
 # udf to convert epoch time to spark TimestampType
-get_timestamp = udf(lambda x : datetime.datetime.fromtimestamp(x/ 1000.0).strftime("%Y-%m-%d %H:%M:%S"))
-           
+get_time = udf(lambda x : datetime.datetime.fromtimestamp(x/ 1000.0).strftime("%H:%M:%S"))
+get_date = udf(lambda x : datetime.datetime.fromtimestamp(x/ 1000.0).strftime("%Y-%m-%d"))
+       
 selectDF_quotes = parsedDF \
         .select(explode(array("quote_data")))\
-        .select("col.*",get_timestamp("col.latestUpdate").cast("timestamp").alias("timestamp"))
-        
-selectDF_quotes = selectDF_quotes.withColumn("timestamp_str",col("timestamp").cast("String"))
-
-selectDF_quotes = selectDF_quotes.select("*",window(selectDF_quotes.timestamp,"1 minutes"))
-
-selectDF_quotes = selectDF_quotes.withColumn("window.timestamp",col("window.start").cast("String"))
-
-#sss.write_console(selectDF_window).awaitTermination()
+        .select("col.*",get_time("col.latestUpdate").cast("String").alias("time")\
+                ,get_date("col.latestUpdate").cast("String").alias("date"))
 
 path = "quotes/quotes"            
 
 selectDF_quotes.writeStream\
-    .option("checkpointLocation",path + "/checkpoint")\
+    .option("checkpointLocation","checkpoint")\
     .outputMode("append")\
     .format("es")\
     .start(path)\
